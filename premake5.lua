2237d9b092f9e9dc853d61c7f3f929b9b8ed2ce5
Luna Catkins <lunarlambda@gmail.com>
2021-02-02 14:52:34 +0100

    update premake file

--- premake5.lua
+++ premake5.lua
@@ -26,4 +26,8 @@ project "mc4-cli"
     kind "ConsoleApp"
 
     files { "mc4-cli/mc4cli.c" }
+    -- Includes should be in libmc4/include/mc4
+    -- Then headers can be included as 'mc4/header.h'
+    -- This'll do for now
+    includedirs { "." }
     links { "libmc4" }
